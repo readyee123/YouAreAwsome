3c5d7db3c407cf5cb518dcc2f35d5f905e38df61
Jacob <pleas@djkl.com>
2024-08-29 20:26:49 -0600

    Initial Commit

---
+++ YouAreAwsome/YouAreAwsomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwsomeApp.swift
+//  YouAreAwsome
+//
+//  Created by Jacob reshetnikov on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwsomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
